ab1a0c393ca0d286a1e5898f9663d96371e685f6
Wennedes de Oliveira Nogueira Junior <wennedesjunior@gmail.com>
2026-06-24 14:26:10 -0300

    Funções básicas de alarme

--- src/Alarme.cpp
+++ src/Alarme.cpp
@@ -1,11 +1,21 @@
 #include "Alarme.hpp"
 
-Alarme::Alarme(std::string Desc, int Id, Parametro& p) {}
+Alarme::Alarme(std::string Desc, int Id, Parametro& p) {
+    this->_Desc = Desc;
+    this->_Id = Id;
+    this->_Parametro = p;
+}
 
 bool Alarme::AtualizarAlarme() { return false; }
 
-bool Alarme::GetEstado() const { return false; }
+bool Alarme::GetEstado() const { 
+    return this->_Estado;
+}
 
-void Alarme::SetEstado(bool Estado) {}
+void Alarme::SetEstado(bool Estado) {
+    this->_Estado = Estado;
+}
 
-void Alarme::SetDesc(std::string desc) {}
+void Alarme::SetDesc(std::string desc) {
+    this->_Desc = desc;
+}
